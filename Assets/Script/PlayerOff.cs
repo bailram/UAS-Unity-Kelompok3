e5e59ca663cdce08d0734add98cdb5893cfdf35d
debbysa <deby.silvia23@gmail.com>
2019-06-12 09:09:13 +0700

    setting collider

---
+++ Assets/Script/PlayerOff.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerOff : MonoBehaviour
+{
+    // private void OnTriggerEnter(BoxCollider2D other)
+    // {
+    //     if (other.tag == "Player")
+    //         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    // }
+    [SerializeField]
+    string strTag;
+
+    private void OnCollisionEnter(Collision Collision)
+    {
+        if (Collision.collider.tag == strTag)
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+    }
+}
